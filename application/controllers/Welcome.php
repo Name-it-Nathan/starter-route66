5c2c6c036f2145719755cfc8be991b775ed21d41
Michael Navarro <navarrom0412@gmail.com>
2018-01-31 10:38:44 -0800

    Completed kilo branch closes #11

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -26,4 +26,11 @@ class Welcome extends Application
 		$this->render();
 	}
 
+        public function show($key)
+        {
+            $this->data['pagebody'] = 'actor';
+            $source = $this->quotes->get($key);
+            $this->data = array_merge($this->data, (array) $source);
+            $this->render();
+        }
 }
